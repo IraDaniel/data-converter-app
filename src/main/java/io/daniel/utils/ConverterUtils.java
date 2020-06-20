6ce641d287391faff4375fec0b95cf0d44968c36
irina.daniel <irina.daniel@amaiz.com>
2020-06-20 16:22:27 +0300

    cosmetic fix

--- src/main/java/io/daniel/utils/ConverterUtils.java
+++ src/main/java/io/daniel/utils/ConverterUtils.java
@@ -10,14 +10,20 @@ import java.util.stream.Collectors;
 public class ConverterUtils {
 
     public static List<OrderResponse> convert(List<ParsedLine> parsedLines, String fileName) {
-        return parsedLines.stream().map(parsedLine -> {
-            Order request = parsedLine.getOrder();
-            if (parsedLine.isSuccess()) {
-                return new OrderResponse(request.getOrderId(), request.getAmount(), request.getComment(), fileName, parsedLine.getLine(), parsedLine.getResult());
-            } else {
-                return new OrderResponse(null, null, null, fileName, parsedLine.getLine(), parsedLine.getResult());
-            }
-        }).collect(Collectors.toList());
+        return parsedLines.stream()
+                .map(parsedLine -> toOrderResponse(fileName, parsedLine))
+                .collect(Collectors.toList());
+    }
+
+    private static OrderResponse toOrderResponse(String fileName, ParsedLine parsedLine) {
+        Order request = parsedLine.getOrder();
+        if (parsedLine.isSuccess()) {
+            return new OrderResponse(request.getOrderId(), request.getAmount(),
+                    request.getComment(), fileName, parsedLine.getLine(), parsedLine.getResult());
+        } else {
+            return new OrderResponse(null, null, null, fileName,
+                    parsedLine.getLine(), parsedLine.getResult());
+        }
     }
 
     private ConverterUtils() {
